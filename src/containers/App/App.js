e20ac6b58dd46da48107cd16c10cc814834ac41f
cryosx <cyruswu.email@gmail.com>
2018-06-12 10:04:28 -1000

    Finished out App boilerplate

---
+++ src/containers/App/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, withRouter } from 'react-router-dom';
+
+import './App.css';
+
+import Header from '../../components/Header/Header';
+import Main from '../../components/Main/Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <div id="app_main">
+          <Header />
+          <Main />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    // user: state.user,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // loadUser: () => {
+    //   dispatch(loadUser());
+    // }
+  };
+};
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
