1f9c48cc8eab1dadaf86510760ed3888fab747d1
cryosx <cyruswu.email@gmail.com>
2018-06-12 09:52:18 -1000

    Added boilerplate code to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
+
+import App from './container/App/App';
+import reducers from './redux/reducers/_reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const boundCompose = compose.bind(null, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__())
+    : boundCompose()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
