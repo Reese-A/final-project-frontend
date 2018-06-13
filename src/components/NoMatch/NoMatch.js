eb832c96e092157d6d7b9b1a2d1359d698728581
Ki Jung Kim <kimkijung@icloud.com>
2018-06-12 14:26:09 -1000

    made a no match component as a catchall for unused routes

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './NoMatch.css';
+
+const NoMatch = (props) => {
+  return (
+    <div>
+      <h1>Page Not Found!</h1>
+      <h3>The page you're looking for does not exist. Please navigate back to the <Link to="/camera">homepage</Link></h3>
+    </div>
+  )
+}
+
+export default NoMatch;
