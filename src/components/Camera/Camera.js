8158c16ddec60c0509658c9501e2e54c6eeea4e9
cryosx <cyruswu.email@gmail.com>
2018-06-13 18:07:12 -1000

    Code cleanup

--- src/components/Camera/Camera.js
+++ src/components/Camera/Camera.js
@@ -22,8 +22,6 @@ class Camera extends React.Component {
       const viewPort = document.getElementById('view_port');
       const shutterBtn = document.getElementById('shutter_btn');
 
-      // console.log(canvas.clientWidth);
-
       const constraints = {
         video: {
           // width: { ideal: 1080 },
@@ -48,20 +46,15 @@ class Camera extends React.Component {
           viewPort.clientWidth,
           viewPort.clientHeight
         );
-        console.log(canvas.toDataURL());
-        const img = new Image();
-        // const data = {
-        //   test: 'test',
-        //   img_url: canvas.toDataURL()
-        // };
 
         const data = new FormData();
-        data.append('img_url', canvas.toDataURL());
+        const uri = canvas.toDataURL().split('base64,')[1];
 
-        fetch('/api/test', {
+        data.append('img_url', uri);
+
+        fetch('/api/images', {
           method: 'POST',
           body: data,
-          // headers: { 'content-type': 'application/json' },
           credentials: 'same-origin'
         })
           .then(res => res.json())
@@ -69,15 +62,7 @@ class Camera extends React.Component {
             console.log(data);
           });
 
-        img.src = canvas.toDataURL();
-        viewPortContainer.innerHTML = '';
-
-        viewPortContainer.appendChild(img);
-
-        canvas.toBlob(blob => {
-          console.log(blob);
-        });
-        viewPort.srcObject.getVideoTracks().forEach(track => track.stop());
+        // viewPort.srcObject.getVideoTracks().forEach(track => track.stop());
       };
 
       // Attach the video stream to the video element and autoplay.
@@ -87,7 +72,7 @@ class Camera extends React.Component {
         .then(stream => {
           const mediaStreamTracks = stream.getVideoTracks();
           const mediaStreamTrack = mediaStreamTracks[0];
-          console.log(mediaStreamTracks);
+
           const capabilities = mediaStreamTrack.getCapabilities();
           const { width, height } = capabilities;
           // this.setState({ width, height });
@@ -99,7 +84,6 @@ class Camera extends React.Component {
           const { width, height } = data;
           const max = width.max > height.max ? width.max : height.max;
           const min = width.min < height.min ? width.min : height.min;
-          console.log(window);
           const constraints = {
             video: {
               facingMode: 'environment',
@@ -107,10 +91,8 @@ class Camera extends React.Component {
               height: { ideal: viewPortContainer.clientHeight }
             }
           };
-          console.log(data);
-          console.log(constraints);
+
           navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-            console.log(stream);
             viewPort.srcObject = stream;
           });
         });
@@ -118,8 +100,6 @@ class Camera extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps);
-    console.log(prevState);
     if (this.state.width !== prevState.width) {
       const viewPortContainer = document.getElementById('view_port_container');
       const viewPort = document.getElementById('view_port');
@@ -134,7 +114,6 @@ class Camera extends React.Component {
         .then(stream => {
           const mediaStreamTracks = stream.getVideoTracks();
           const mediaStreamTrack = mediaStreamTracks[0];
-          console.log(mediaStreamTracks);
           const capabilities = mediaStreamTrack.getCapabilities();
           const { width, height } = capabilities;
           // this.setState({ width, height });
@@ -146,7 +125,6 @@ class Camera extends React.Component {
           const { width, height } = data;
           const max = width.max > height.max ? width.max : height.max;
           const min = width.min < height.min ? width.min : height.min;
-          console.log(window);
           const constraints = {
             video: {
               facingMode: 'environment',
@@ -154,10 +132,8 @@ class Camera extends React.Component {
               height: { ideal: viewPortContainer.clientHeight }
             }
           };
-          console.log(data);
-          console.log(constraints);
+
           navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-            console.log(stream);
             viewPort.srcObject = stream;
           });
         });
