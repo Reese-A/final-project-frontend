69ed159715a2115972f4c0741e24871be5e2a279
cryosx <cyruswu.email@gmail.com>
2018-06-14 09:38:14 -1000

    Created routes to go to each component for testing

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,8 @@ import Home from '../Home/Home';
 import Camera from '../Camera/Camera';
 import NoMatch from '../NoMatch/NoMatch';
 import Registration from '../Registration/Registration';
+import SearchForm from '../SearchForm/SearchForm';
+import AddFood from '../AddFood/AddFood';
 
 import './Main.css';
 
@@ -13,14 +15,17 @@ class Main extends React.Component {
     super(props);
     this.state = {};
   }
-  componentDidMount() { }
+  componentDidMount() {}
 
   render() {
     return (
       <main id="main">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/camera" component={Camera} />
+          <Route exact path="/add/camera" component={Camera} />
+          <Route exact path="/add/search" component={SearchForm} />
+          <Route exact path="/add" component={AddFood} />
+
           <Route exact path="/register" component={Registration} />
           <Route component={NoMatch} />
         </Switch>
