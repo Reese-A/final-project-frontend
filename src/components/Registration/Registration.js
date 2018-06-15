b1d7a1a27e6052a057de43facecf43d2fb902a9f
Jace Mitsuka <PJace@BigMac.local>
2018-06-14 14:19:53 -1000

    created registration component

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,6 +1,9 @@
  import React from 'react';
  import { Switch, Route, Redirect } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
+import AccountForm from '../../containers/AccountForm/AccountForm';
+import ProfileForm from '../../containers/ProfileForm/ProfileForm';
+import FitnessForm from '../../containers/FitnessForm/FitnessForm';
+import ReviewForm from '../../containers/ReviewForm/ReviewForm';
  //ProfileForm, FitnessForm, ReviewForm
 
  class Registration extends React.Component {
@@ -9,21 +12,31 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 
     this.state = {
       page: null
-    }
+    };
     this.nextPage = this.nextPage.bind(this);
     this.previousPage = this.previousPage.bind(this);
+    this.firstPage = this.firstPage.bind(this);
+    this.secondPage = this.secondPage.bind(this);
   }
 
   componentDidMount(){
     this.setState({ page: 1 });
-  }
+  };
 
   nextPage() {
-    this.setState({ page: this.state.page + 1})
+    this.setState({ page: this.state.page + 1 });
   };
 
   previousPage() {
-    this.setState({ page: this.state.page - 1})
+    this.setState({ page: this.state.page - 1 });
+  };
+
+  firstPage() {
+    this.setState({ page: 1 });
+  };
+
+  secondPage() {
+    this.setState({ page: 2  });
   };
 
 
@@ -32,15 +45,33 @@ import RegisterForm from '../RegisterForm/RegisterForm';
       case 1:
         return(
           <div id="registration">
-          <RegisterForm nextPage={this.nextPage}/>
+          <AccountForm nextPage={this.nextPage}/>
           </div> 
+        )
+      case 2: 
+        return(
+          <div id="registration">
+          <ProfileForm previousPage={this.previousPage} nextPage={this.nextPage} />
+          </div>
+        )
+      case 3: 
+        return(
+          <div id="registration">
+          <FitnessForm previousPage={this.previousPage} nextPage={this.nextPage} />
+          </div>
+        )
+      case 4: 
+        return(
+          <div id="registration">
+          <ReviewForm previousPage={this.previousPage} firstPage={this.firstPage} secondPage={this.secondPage}/>
+          </div>
         )
       default:
         return(
           <div>hello world</div>
         ) 
-    }
-  }
- }
+    };
+  };
+ };
 
  export default Registration;
