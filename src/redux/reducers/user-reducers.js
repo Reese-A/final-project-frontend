9c5dc66ca11002c41a0633b6eeaaf9a548fd6eb4
Ki Jung Kim <kimkijung@icloud.com>
2018-06-15 14:46:42 -1000

    added login action to reroute to dashboard

--- src/redux/reducers/user-reducers.js
+++ src/redux/reducers/user-reducers.js
@@ -1,15 +1,13 @@
 import { LOAD_USER, LOGIN_USER } from '../actions/user-actions';
 
-const initialState = {
-  online: false,
-};
+const initialState = {};
 
 const user = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_USER:
-      return { ...state, user: action.user };
+      return { ...state, ...action.user };
     case LOGIN_USER:
-      return { ...state, online: action.online }
+      return { ...state, ...action.user }
     default:
       return state;
   }
