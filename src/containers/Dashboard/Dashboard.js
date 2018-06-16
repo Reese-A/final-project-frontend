b5bebc0ff9b108693cbccd11257413b092313f59
Ki Jung Kim <kimkijung@icloud.com>
2018-06-15 18:18:05 -1000

    added an add link to dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { loadUser } from '../../redux/actions/user-actions';
+import { Redirect, Link } from 'react-router-dom';
 
 import Header from '../../components/Header/Header';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidMount() {
-    if (this.props.user.id) {
-      this.props.loadUser(this.props.user.id);
-    }
   }
 
   render() {
@@ -25,11 +18,12 @@ class Dashboard extends React.Component {
     return (
       <div id="dashboard">
         <Header />
-        <div id="dashbordWrap">
+        <div id="dashboardWrap">
           <div id="intake">
             <div id="calories">Calories Consumed: </div>
             <div id="chart">Macronutrients: </div>
           </div>
+          <div><Link to="/add">Add</Link></div>
         </div>
       </div>
     )
@@ -42,12 +36,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    loadUser: (id) => {
-      dispatch(loadUser(id));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, null)(Dashboard);
