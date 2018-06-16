4cfa4ec4a8f865f4b7d2c30ef37e4d7ef486ae3f
cryosx <cyruswu.email@gmail.com>
2018-06-15 15:17:02 -1000

    Changed search input to type text and initialized food item

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,20 +7,54 @@ class SearchForm extends React.Component {
     super(props);
     this.state = {
       width: 0,
-      search: ''
+      search: '',
+      food: {
+        calories: 0,
+        carb: 0,
+        category: {
+          created_at: '',
+          id: 0,
+          name: '',
+          updated_at: ''
+        },
+        category_id: 0,
+        created_at: '',
+        fat: 0,
+        id: 0,
+        name: '',
+        popularity: 0,
+        protein: 0,
+        serving_grams: 0,
+        serving_size: '',
+        updated_at: ''
+      }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.changeHandler = this.changeHandler.bind(this);
   }
   componentDidMount() {
-    this.setState({ search: this.props.item });
+    this.setState({ search: this.props.item }, () => {
+      if (this.state.search) {
+        return fetch(`/api/foods/${this.state.search}`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          credentials: 'same-origin'
+        })
+          .then(res => res.json())
+          .then(food => {
+            console.log(food);
+          });
+      }
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {}
 
   changeHandler(event) {
     const { value, name } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value.toLowerCase() });
   }
 
   handleSubmit(event) {
@@ -46,7 +80,7 @@ class SearchForm extends React.Component {
           <div className="add_food_search_container">
             <input
               value={this.state.search}
-              type="search"
+              type="text"
               name="search"
               placeholder="Search for something"
               onChange={this.changeHandler}
