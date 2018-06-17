043651cf6e67421f40a66e1b57dd2f7c1d0222a4
Ki Jung Kim <kimkijung@icloud.com>
2018-06-16 14:47:30 -1000

    add food to dishes action/reducer created

--- src/redux/reducers/_reducers.js
+++ src/redux/reducers/_reducers.js
@@ -4,12 +4,14 @@ import user from './user-reducers';
 import gender from './gender-reducers';
 import goal from './goal-reducers';
 import activityLevel from './activity-level-reducers';
-import registrationForm from './reg-form-reducers'; 
+import registrationForm from './reg-form-reducers';
+import food from './food-reducers';
 
 export default combineReducers({
   user,
-  gender, 
+  gender,
   goal,
   activityLevel,
-  registrationForm
+  registrationForm,
+  food
 });
