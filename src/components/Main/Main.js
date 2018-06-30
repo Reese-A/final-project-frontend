efeea7b782063b9f2b1f2bc26289bbd0a83ca7ad
cryosx <cyruswu.email@gmail.com>
2018-06-29 16:03:17 -1000

    DishDetail route

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,7 @@ import Registration from '../Registration/Registration';
 import Dashboard from '../../containers/Dashboard/Dashboard';
 import AddFood from '../../containers/AddFood/AddFood';
 import Settings from '../../containers/Settings/Settings';
-
+import DishDetail from '../../containers/DishDetail/DishDetail';
 import './Main.css';
 
 class Main extends React.Component {
@@ -23,6 +23,7 @@ class Main extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dish/:id" component={DishDetail} />
           <Route exact path="/add" component={AddFood} />
           <Route exact path="/settings" component={Settings} />
           <Route exact path="/register" component={Registration} />
